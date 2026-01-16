dedc1d622916ef08dfa4d60d9c14c63c9ada1856
demvlad <demvlad2@yandex.ru>
2026-01-16 23:33:01 +0300

    Esp32 program can work in AP or STA mode. Web page allows to change settings

--- src/config.h
+++ src/config.h
@@ -1,18 +1,28 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 #include <Arduino.h>
-
-// WIFI access point setup
-const char* ap_ssid = "mavlink";
-const char* ap_password = "12345678";  // It needs 8 symbols
-
+// 4E:52:A6:FB:23:EA my MAC
 const uint16_t UDP_PORT = 14550; // GCS UDP port
+const char apSSID[] = "mavlink";
+const char apPassword[] = "12345678";
+typedef enum {
+    AP_WIFI_MODE = 0,      // Точка доступа
+    STA_WIFI_MODE = 1      // Клиент
+} WiFiOperationMode;
 
 struct Config {
-  uint8_t mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // Default MAC
-  String wifi_mode = "ap";
-  String wifi_ssid = "mavlink";
-  String wifi_password = "12345678";
+  uint8_t customMAC[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; // Custom MAC
+  WiFiOperationMode wifiMode;
+  char wifiSSID[16];
+  char wifiPassword[32];
+  uint8_t wifiChannel;
 };
+
+void saveMacToStorage();
+void loadMacFromStorage();
+void saveWifiToStorage();
+void loadWifiFromStorage();
+String macToString(uint8_t* mac);
+
 extern Config config;
 #endif
--- src/web-server.h
+++ src/web-server.h
@@ -3,7 +3,5 @@
 
 void webSwerverSetup();
 void webServerRun();
-void loadMacFromStorage(uint8_t mac[6]);
-void loadWifiFromStorage();
 
 #endif
